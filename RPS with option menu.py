ecd856afc1aef25cd3b5cb9161ef705fc82d2105
Iliya-Yadegari <naruto3@email.com>
2021-04-11 19:35:22 +0100

    Update RPS with option menu.py

--- RPS with option menu.py
+++ RPS with option menu.py
@@ -51,8 +51,7 @@ r.set(optionLst[0])
 
 main_frm = LabelFrame(window)
 option_label = Label(main_frm,text = 'Chosse your move ===>').grid(row = 0, column = 0, padx = 10, pady = 10)
-drop = OptionMenu(main_frm,r,*optionLst).grid(row = 0, column = 1, padx = 10, pady = 10)
-submit_btn = Button(main_frm,text = 'Submit',width = 20, height = 3,command = Prs).grid(row = 1, column = 0, padx = 10, pady = 10)
+drop = OptionMenu(main_frm,r,*optionLst).grid(row = 0, column = 1, padx = 10, pady = 10)submit_btn = Button(main_frm,text = 'Submit',width = 20, height = 3,command = Prs).grid(row = 1, column = 0, padx = 10, pady = 10)
 
 main_frm.grid(row = 0, column = 0, padx = 10, pady = 10)
 
